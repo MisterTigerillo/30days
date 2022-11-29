a433782814efdc8ad2369beab8460b0f1c9ecc03
Dmytro Gobosh <88391310+MisterTigerillo@users.noreply.github.com>
2022-11-29 12:42:32 +0200

    +misc

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="notificationsFrame">
+        <div className="panel">
+          {" "}
+          <Header title="Timeline" />
+          {/* <Content/> */}
+        </div>
+      </div>
+    );
+  }
+}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header"></header>
+//     </div>
+//   );
+// }
+
+// export default App;
